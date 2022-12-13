3860dc75e30bf1377ed756286f4c847cdb2e8177
Codertjay <codertjay@gmail.com>
2022-12-13 19:26:53 +0100

    fixed cors

--- leadboard_api/settings/base.py
+++ leadboard_api/settings/base.py
@@ -204,7 +204,7 @@ CORS_ALLOWED_ORIGIN_REGEXES = [
     r"^https://\w+\.instincthub\.com$",
     "http://localhost:3000",
     "http://localhost:3001",
-    "http://127.0.0.1:5500",
+    # "http://127.0.0.1:5500",
 ]
 
 # CORS_ALLOW_ALL_ORIGINS = True
