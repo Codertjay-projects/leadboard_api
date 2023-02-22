2a5291ce43fb3b45746f81b78894d44b3e21d867
Codertjay <codertjay@gmail.com>
2023-02-22 16:45:04 +0100

    added lead list with staff on params

--- companies/tasks.py
+++ companies/tasks.py
@@ -1,3 +1,14 @@
+from celery import shared_task
 
 
+@shared_task
+def modify_all_company_send_email_count():
+    """
+    this is used to modify all the company email_send_count to zero every first of the month
+    :return:
+    """
+    from .models import Company
 
+    for item in Company.objects.all():
+        item.email_sent_count = 0
+        item.save()
--- leadboard_api/lead_celery.py
+++ leadboard_api/lead_celery.py
@@ -21,12 +21,14 @@ app.conf.broker_url = config("CELERY_BROKER_URL")
 #  this is used to make an automation either send mail during a specific time
 #  or delete some stuff or more
 app.conf.beat_schedule = {
-
+    # This modify the companies email sent count to one every first day of the month
+    "modify_all_company_send_email_count": {
+        "task": 'companies.tasks.modify_all_company_send_email_count',
+        "schedule": crontab(day_of_month=1),
+    },
 }
 
 
 @app.task(bind=True)
 def debug_task(self):
     print(f'Request: {self.request!r}')
-
-
