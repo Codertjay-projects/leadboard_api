a050461cb76b6d328dc718382f8158462f850f33
Codertjay <codertjay@gmail.com>
2023-04-03 16:20:40 +0100

    added leads and also fix auth

--- leads/views.py
+++ leads/views.py
@@ -265,7 +265,7 @@ class LeadUploadAPIView(APIView):
         try:
             for item in data:
                 group, group_created = Group.objects.get_or_create(
-                    title=item.get("lead_type").upper(),
+                    title=item.get("g").upper(),
                     company=company)
                 # Get or create the lead contact
                 lead_contact = LeadContact.objects.filter(email=item.get("email"), company=company).first()
